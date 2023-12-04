400c6bdcc8996536034aa821ad9e15e870b695b5
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-12-03 22:49:34 -0300

    Initial Commit

---
+++ backend/config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :backend, Backend.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "backend_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :backend, BackendWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "KWyWvanHS2OPLlATfEcbD/FD/IwGle9K92x5vRaRDJC4ran/iKayVNCoU0lIuI4c",
+  server: false
+
+# In test we don't send emails.
+config :backend, Backend.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
